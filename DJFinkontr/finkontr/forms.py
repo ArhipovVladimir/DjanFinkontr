85d3d5433e2f8cd6b36e2dd0f73830b6248c2d53
ArhipovVS <arhipof05@yndex.ru>
2023-09-11 22:00:43 +0700

    Shablon2

--- DJFinkontr/finkontr/forms.py
+++ DJFinkontr/finkontr/forms.py
@@ -54,3 +54,24 @@ class ManyFieldsFormWidget(forms.Form):
 
 class ImageForm(forms.Form):
      image = forms.ImageField()
+
+# из домашнего задания
+
+class ProductForm(forms.Form):
+    # products = forms.ModelChoiceField(label='Продукты', queryset=Product.objects.all())
+    name = forms.CharField(max_length=100,
+                           widget=forms.TextInput(attrs={'class': 'form-control',
+                                                         'placeholder': 'Введите назв продукта'}))
+    description = forms.CharField(max_length=100,
+                                  widget=forms.TextInput(attrs={'class': 'form-control',
+                                                         'placeholder': 'Введите описание продукта'}))
+
+    price = forms.DecimalField(max_digits=8, decimal_places=2)
+    quantity = forms.IntegerField()
+    image = forms.ImageField()
+
+
+class ProductFormUpdate(ProductForm):
+    products = forms.ModelChoiceField(label='Продукты', queryset=Product.objects.all())
+
+
--- DJFinkontr/finkontr/views.py
+++ DJFinkontr/finkontr/views.py
@@ -99,3 +99,77 @@ def author_posts(request, author_id):
 def post_full(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'less_3_tmpl/post_full.html', {'post': post})
+
+# из домашнего задания
+
+import form as form
+from django.shortcuts import render
+from .forms import ProductForm, ProductFormUpdate
+from hw3.models import Product
+import logging
+from django.core.files.storage import FileSystemStorage
+
+logger = logging.getLogger(__name__)
+
+
+def add_product(request):
+    name = 'Добавление продукта'
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        message = 'Ошибка в данных'
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            price = form.cleaned_data['price']
+            quantity = form.cleaned_data['quantity']
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+            # logger.info(f'Получили {name=}, {email=}, {age=}.')
+            product = Product(name=name, description=description, price=price, quantity=quantity, image=image.name)
+            product.save()
+            message = 'product сохранён'
+    else:
+        form = ProductForm()
+        message = 'Заполните форму'
+
+    return render(request, 'hw4/product.html', {'form': form, 'message': message, 'name': name})
+
+
+def update_product(request):
+    name = 'Изменение продукта'
+    if request.method == 'POST':
+        form = ProductFormUpdate(request.POST, request.FILES)
+        message = 'Ошибка в данных'
+        if form.is_valid():
+            product = form.cleaned_data['products']
+            old_product = Product.objects.filter(pk=product.pk).first()
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            price = form.cleaned_data['price']
+            quantity = form.cleaned_data['quantity']
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+            # logger.info(f'Получили {name=}, {email=}, {age=}.')
+            # old_product(name=name, description=description, price=price, quantity=quantity, image=image.name)
+            old_product.name = name
+            old_product.description = description
+            old_product.price = price
+            old_product.quantity = quantity
+            old_product.image = image.name
+            old_product.save()
+            message = 'product сохранён'
+    else:
+        form = ProductFormUpdate()
+        message = 'Заполните форму'
+
+    return render(request, 'hw4/product.html', {'form': form, 'message': message, 'name': name})
+
+
+def index(request):
+    return render(request, 'hw4/index.html')
+
+
+def about(request):
+    return render(request, 'hw4/about.html')
